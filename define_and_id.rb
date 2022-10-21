d523fb4804c3fad941b6028ca5497cf0b8b3a965
Jade Stewart <jadekstewart3@gmail.com>
2022-10-21 12:43:53 -0600

    Complete define and id exercise

--- define_and_id.rb
+++ define_and_id.rb
@@ -11,17 +11,27 @@
 
 
 "Hello World".include?("Hello")
-
+# The include? method is called on the string object "Hello World".
+# One argument is passed; include? is asking if the string "Hello" is present in the string object "Hello World".
+# The return value is true.
 
 "Hello World".end_with?("Hello")
-
+# The end_with? method is called on the string object "Hello World".
+# One argument is passed; end_with? is asking if the string object "Hello World" ends with the argument "Hello".
+# The return value is false.
 
 "Hello World".end_with?("rld")
-
+# The end_with? method is called on the string object "Hello World".
+# One argument is passed; end_with? is asking if the string object "Hello World" ends with the argument "rld".
+# The return value is true
 
 12.even?
-
+# The even? method is called on the integer 12.
+# No arguments are passed; even? is asking if the integer is even.
+# The return value is true.
 
 18.next
-
+# The next method is called on the integer 18.
+# No arguments are passed; next has one job, which is to return the next whole integer.
+# The return value is 19.
 
